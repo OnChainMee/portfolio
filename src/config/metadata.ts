8dfefb57ea09c0e27323c0876a9689a7648f9075
2-rust <dillonking130@gmail.com>
2025-11-26 10:00:43 -0500

    change tg

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -20,11 +20,11 @@ export const metaData: Metadata = {
         ],
     },
     title:
-        "T-rustdev | Software Developer | React, Next.js, Nest.js in Himachal Pradesh",
+        "T-rustdev | Software Developer | React, Next.js, Nest.js",
     description:
         "Discover T-rustdev, Discover T-rustdev, Passionate and seasoned solution architect and software engineer with a robust expertise in web and blockchain technologies. Over the past eight years, I've effectively leveraged technology to craft innovative digital solutions. I thrive on embracing new challenges and navigating through obstacles to deliver impactful results. Excited about pushing the boundaries of technology and contributing to the ever-evolving landscape of digital solutions.",
     keywords:
-        "T-rustdev, Blockchain and Full-Stack Developer, Solana Developer, EVM developer, Bitcoin Developer, ReactJS Developer, NextJS Developer, NestJS Developer, Remix Developer, Shopify Apps, AI Innovation, Web Development, Portfolio, React developer, Next js developer, Nest js developer, Himachal Pradesh, India, PostgreSQL, Strapi, Sanity, Supabase, Appwrite, TailwindCSS",
+        "T-rustdev, Blockchain and Full-Stack Developer, Solana Developer, EVM developer, Bitcoin Developer, ReactJS Developer, NextJS Developer, NestJS Developer, Remix Developer, Shopify Apps, AI Innovation, Web Development, Portfolio, React developer, Next js developer, Nest js developer, PostgreSQL, Strapi, Sanity, Supabase, TailwindCSS",
     robots:
         "index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1",
     icons: {
